3048cdf57b12ac373faf59f6f94449aecb09793c
Ehbraheem <ehbraheem@gmail.com>
2017-03-14 16:44:59 +0100

    Added product page

--- client/src/javascripts/pay/product/product.component.js
+++ client/src/javascripts/pay/product/product.component.js
@@ -15,11 +15,25 @@
 
     templateUrl.$inject = ["pay.config.APP_CONFIG"];
 
+    ProductController.$inject = ["pay.product.Products"]
+
     function templateUrl (APP_CONFIG) {
         return APP_CONFIG.product_html;
     }
 
-    function ProductController () {
+    function ProductController (Products) {
+
+        var $ctrl = this;
+        $ctrl.allProducts;
+
+        activate();
+        return;
+        /////////////////////////////////
+        function activate() {
+            $ctrl.allProducts = Products.allProducts()[1]["S.T.E.M"];
+            console.log($ctrl.allProducts["S.T.E.M"]);
+            window.allProducts = $ctrl.allProducts;
+        }
 
     };
 })();
--- client/src/javascripts/pay/product/product.service.js
+++ client/src/javascripts/pay/product/product.service.js
@@ -2,4 +2,88 @@
  * Created by Ehbraheem on 14/03/2017.
  */
 
-()();
+(function () {
+
+    "use strict";
+
+    angular
+        .module("pay.product")
+        .service("pay.product.Products", ProductService);
+
+    ProductService.$inject = [];
+
+    function ProductService() {
+
+        var service = this;
+        service.products = [
+
+            {
+                "Core Lab": [
+                    {
+                        "name": "App Lab",
+                        "price": 200000,
+                        "duration": "4 months"
+                    },
+                    {
+                        "name": "Workforce Lab",
+                        "price": 400000,
+                        "duration": "4 months"
+                    }
+                ]
+            },
+            {"S.T.E.M" : [
+                {
+                    "name" : "Robotics",
+                    "price" : 35000,
+                    "duration": "1 Week"
+                },
+                {
+                    "name" : "Creative Computing",
+                    "price" : 35000,
+                    "duration": "1 Week"
+                },
+                {
+                    "name": "Science Lab (Python)",
+                    "price": 50000,
+                    "duration": "2 Weeks"
+                }
+            ]
+            } ,
+            {
+                "Corporate training": [
+                    {
+                        "name": "Social media & marketing",
+                        "price": 50000,
+                        "duration": "1 Week"
+                    },
+                    {
+                        "name": "Business inteligent",
+                        "price": 50000,
+                        "duration": "1 Week"
+                    }
+                ]
+            }
+        ];
+
+        service.allProducts = allProducts;
+
+        service.getProductList = getProductList;
+
+        service.getSingleProduct = getSingleProduct;
+
+        return;
+        ///////////////////////
+
+        function allProducts() {
+            return service.products;
+        }
+
+        function getProductList(index) {
+            return service.products[key];
+        }
+
+        function getSingleProduct(topIndex, productIndex) {
+            return service.products[topIndex][productIndex];
+        }
+    }
+})();
